54433ab538c106e6b0ddb1023631bc4023886ea9
theholygun2 <davidstefanusst@gmail.com>
2022-04-06 15:08:36 +0700

    initial commit

---
+++ commands/profile.js
@@ -0,0 +1,49 @@
+exports.run = (client, message) => {
+    let user = message.author
+    
+    let userAvatarPath = `avatars/${user.id}/${user.avatar}`
+    let avatarUrl = `https://cdn.discordapp.com/${userAvatarPath}.jpg`
+
+    message.channel.send({ embeds: [{
+        color: 3447003,
+        author: {
+            name: user.username,
+            icon_url: "https://i.imgur.com/NQ3knjY.jpeg"
+    },
+    thumbnail: {
+      url: avatarUrl
+    },
+    image: {
+    url: avatarUrl
+    },
+    title: "",
+    url: "",
+    description: "Description",
+    fields: [{
+    name: "This is a single field title, it can hold 256 characters",
+    value: "This is a field value, it can hold 1024 characters.",
+    inline: false
+    },
+    {
+    name: "\u200b",
+    value:"\u200b"
+    }],
+    timestamp: new Date(),
+    footer: {
+    text: "This is the footer text, it can hold 2048 characters"
+    }
+    }]});
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Bot Owner"
+  };
+  
+exports.help = {
+    name: "profile",
+    category: "System",
+    description: "Display your profile",
+    usage: "profile"
+};
