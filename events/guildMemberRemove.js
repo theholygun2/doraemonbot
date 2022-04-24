b2a6ba5966b76b7f4b0972a9e7ca46677aa5cb28
theholygun2 <davidstefanusst@gmail.com>
2022-04-24 16:58:27 +0700

    add member remove

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,4 +2,13 @@ const { Permissions } = require("discord.js");
 
 module.exports = (member) => {
     const entry = await message.guild.fetchAuditLogs({ type: "MEMBER_KICK" }).then(audit => audit.entries.first())
+
+    const { executor, target } = entry;
+
+    if (target.id === member.id) {
+        console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}`);
+    }
+    else {
+        console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}`);
+    }
 }
