0f91a989822d2690532edb210596ba3428ae7d80
theholygun2 <davidstefanusst@gmail.com>
2022-06-26 21:05:52 +0700

    fix

--- commands/add.js
+++ commands/add.js
@@ -1,23 +1,23 @@
 const { Users, Courses } = require('../dbObjects.js');
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, User } = require('discord.js')
+const { Op } = require('sequelize');
 
 exports.run = async (client, message, args) => {
   
+  console.log(args.length)
+  if(args.length != 2) return
   const [name, kode] = args
   const user = message.author
   let link = "https://elearning.pnj.ac.id/course/view.php?id="
-
   
   if(name.length > 10) return message.reply("subject must not be longer than 10 characters/digit")
   isNaN(kode) ? link=kode : link=`${link}${kode}`
   
-  
+  const db_user = await Users.findOne({where: {user_id: user.id}})
   var newCours = Courses.build({ name: name, link: link, user_id: user.id});
-  const db_user = await Users.findOne({where: { user_id: user.id}})
   if(!db_user){
       try {
-        await Users.create({user_id: user.id, username: user.username})
-        await newCours.save()
+        await Users.create({user_id: user.id, username: user.tag}).then(newCours.save())
         return message.reply(`Link saved to ${newCours.name}`)
       } catch (error) {
         if (error.name === 'SequelizeUniqueConstraintError') {
@@ -25,19 +25,13 @@ exports.run = async (client, message, args) => {
           }
       }
     }
-    else{
-      const target = await Courses.findOne({where: {user_id: db_user.user_id, name: newCours.name}})
-      if(target){
-        target.set({link: newCours.link})
-        await target.save()
-        return message.reply(`Your link is edited!`)
+    const row = await Courses.findOne({where: {user_id: user.id, name: {[Op.like]: name}}})
+      if(row) {
+        row.set({link: link})
+        await row.save()
+        return message.reply(`Link inside ${name} has been changed`)
       }
-      else{
-        await newCours.save()
-        return message.reply(`Your link has been saved to: ${newCours.name}. use ~c to show`)
-      }
-    }
-    return console.log("something wrong men")
+    return message.reply(`Link has been saved to ${name}`)
 }
 
 exports.conf = {
